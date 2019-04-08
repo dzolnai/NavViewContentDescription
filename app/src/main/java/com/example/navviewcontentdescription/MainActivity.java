e606bf51a98f9f6f7cd7bb7bad48b437f9cfb9db
Dániel Zolnai <zoiddani@gmail.com>
2019-04-08 13:31:15 +0200

    First commit

---
+++ app/src/main/java/com/example/navviewcontentdescription/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.navviewcontentdescription;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NavigationView navView = findViewById(R.id.navigation);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Toast.makeText(MainActivity.this, "Item clicked: " + item.getTitle(), Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+    }
+}
